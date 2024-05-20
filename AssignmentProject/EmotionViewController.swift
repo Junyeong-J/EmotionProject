fe99b97b33315b1153bc48746a6c334ed80ecfb7
JunYeong-J <eeot2827@gmail.com>
2024-05-21 00:55:43 +0900

    변수명 수정

--- AssignmentProject/EmotionViewController.swift
+++ AssignmentProject/EmotionViewController.swift
@@ -13,8 +13,8 @@ class EmotionViewController: UIViewController {
     var countList = [0,0,0,0,0,0,0,0,0]
     let textList = ["행복해", "사랑해", "좋아해", "당황해", "속상해", "우울해", "심심해", "짜증나", "눈물나"]
     
-    @IBOutlet var buttonList: [UIButton]!
-    @IBOutlet var labelList: [UILabel]!
+    @IBOutlet var buttonLists: [UIButton]!
+    @IBOutlet var labelLists: [UILabel]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,15 +25,15 @@ class EmotionViewController: UIViewController {
     
     func setUI() {
         
-        settingButtonOrLabel(button: buttonList[0], label: labelList[0], imageName: "slime1", textLabel: "행복해 0")
-        settingButtonOrLabel(button: buttonList[1], label: labelList[1], imageName: "slime2", textLabel: "사랑해 0")
-        settingButtonOrLabel(button: buttonList[2], label: labelList[2], imageName: "slime3", textLabel: "좋아해 0")
-        settingButtonOrLabel(button: buttonList[3], label: labelList[3], imageName: "slime4", textLabel: "당황해 0")
-        settingButtonOrLabel(button: buttonList[4], label: labelList[4], imageName: "slime5", textLabel: "속상해 0")
-        settingButtonOrLabel(button: buttonList[5], label: labelList[5], imageName: "slime6", textLabel: "우울해 0")
-        settingButtonOrLabel(button: buttonList[6], label: labelList[6], imageName: "slime7", textLabel: "심심해 0")
-        settingButtonOrLabel(button: buttonList[7], label: labelList[7], imageName: "slime8", textLabel: "짜증나 0")
-        settingButtonOrLabel(button: buttonList[8], label: labelList[8], imageName: "slime9", textLabel: "눈물나 0")
+        settingButtonOrLabel(button: buttonLists[0], label: labelLists[0], imageName: "slime1", textLabel: "행복해 0")
+        settingButtonOrLabel(button: buttonLists[1], label: labelLists[1], imageName: "slime2", textLabel: "사랑해 0")
+        settingButtonOrLabel(button: buttonLists[2], label: labelLists[2], imageName: "slime3", textLabel: "좋아해 0")
+        settingButtonOrLabel(button: buttonLists[3], label: labelLists[3], imageName: "slime4", textLabel: "당황해 0")
+        settingButtonOrLabel(button: buttonLists[4], label: labelLists[4], imageName: "slime5", textLabel: "속상해 0")
+        settingButtonOrLabel(button: buttonLists[5], label: labelLists[5], imageName: "slime6", textLabel: "우울해 0")
+        settingButtonOrLabel(button: buttonLists[6], label: labelLists[6], imageName: "slime7", textLabel: "심심해 0")
+        settingButtonOrLabel(button: buttonLists[7], label: labelLists[7], imageName: "slime8", textLabel: "짜증나 0")
+        settingButtonOrLabel(button: buttonLists[8], label: labelLists[8], imageName: "slime9", textLabel: "눈물나 0")
         
     }
     
@@ -51,7 +51,7 @@ class EmotionViewController: UIViewController {
         
         countList[sender.tag] += 1
         
-        labelList[sender.tag].text = "\(textList[sender.tag])  \(countList[sender.tag])"
+        labelLists[sender.tag].text = "\(textList[sender.tag])  \(countList[sender.tag])"
         
     }
     
